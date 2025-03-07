b2887d37d91b787d739eef6bd124a7636fa80758
starrydriver <150936096+starrydriver@users.noreply.github.com>
2025-03-07 15:59:34 +0800

    login++

--- js/index.js
+++ js/index.js
@@ -15,11 +15,29 @@ function loadSidebar() {
             sidebarContainer.innerHTML = '侧栏加载失败';
         });
 }
+//加载出登陆菜单
+function loadLogin() {
+    const myLogin = document.getElementById('myLogin');
+    fetch('login.html')
+        .then(response => response.text())
+        .then(html => {
+            myLogin.innerHTML = html;
+            setTimeout(() => {
+                myLogin.style.width = "1000px";
+            }, 10);
+        })
+        .catch(error => {
+            console.error('加载侧栏失败:', error);
+            myLogin.innerHTML = '侧栏加载失败';
+        });
+}
 //点击ESC关闭侧栏
 document.addEventListener('keydown', function(event) {
     if (event.key == "Escape") {
         document.getElementById('myRigister').style.width = "0";
+        document.getElementById('myLogin').style.width = "0";
     }
 });
 // 绑定按钮点击事件
 document.getElementById('openBtn').addEventListener('click', loadSidebar);
+document.getElementById('myBtn_Login').addEventListener('click', loadLogin);
---
+++ js/login.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+	const img = document.getElementById('myImg2');
+	setTimeout(() => {
+		img.style.left = "500px";
+		img.style.opacity = "0.9";
+	}, 10);
+});
