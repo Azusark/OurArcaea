9e6c4617396fc16bf95a00d055adfeb285323a5c
Azusark <3082614246@qq.com>
2025-03-16 01:30:07 +0800

    返回按钮

--- js/page2.js
+++ js/page2.js
@@ -22,21 +22,25 @@ let velocity = 0;
 let lastTime = 0;
 let lastScroll = 0;
 let rafId;
+
 //加载专辑
 function loadAlbum() {
   var myAlbumsContainer = new Array();
   var myAlbumsItem = new Array();
   for (var i = 1; i <= 16; i++) {
-    myAlbumsContainer[i]=document.createElement('div');
+    myAlbumsContainer[i] = document.createElement('div');
     myAlbumsContainer[i].className = 'item-container';
-    myAlbumsItem[i]=document.createElement('img');
-    myAlbumsItem[i].src = "../icon/album/album"+ i + ".png";
+    myAlbumsItem[i] = document.createElement('img');
+    myAlbumsItem[i].src = "../icon/album/album" + i + ".png";
     console.log(myAlbumsItem[i].src);
     myAlbumsItem[i].className = 'item';
     myAlbumsContainer[i].appendChild(myAlbumsItem[i]);
     content.appendChild(myAlbumsContainer[i]);
   }
 }
+
+
+
 loadAlbum();
 // 滑动鼠标事件
 container.addEventListener('mousedown', (e) => {
@@ -111,11 +115,39 @@ function applyMomentum() {
 }
 //回弹效果
 //返回主页
-document.getElementById('backButton').addEventListener('click', function() {
+document.getElementById('backbutton').addEventListener('click', function() {
   channel.postMessage({ type: 'PlayAnimation' });
   console.log('开始游戏');
   setTimeout(() => {
       //跳转到其他界面
       window.location.href = "../html/index.html";
       }, 1600);
+});
+
+//返回按钮
+// 获取按钮和图片元素
+const backButton = document.getElementById('backbutton');
+const hoverImage = backButton.querySelector('.hover-image');
+
+// 定义默认图片和悬停图片路径
+const defaultImage = '../icon/backbutton.png';
+const hoverImageSrc = '../icon/backbutton-pressed.png';
+
+// 监听鼠标悬停事件
+backButton.addEventListener('mouseenter', () => {
+    hoverImage.src = hoverImageSrc; // 切换到有阴影的图片
+});
+
+backButton.addEventListener('mouseleave', () => {
+    hoverImage.src = defaultImage; // 切换回默认图片
+});
+
+// 返回主页
+backButton.addEventListener('click', function() {
+    channel.postMessage({ type: 'PlayAnimation' });
+    console.log('开始游戏');
+    setTimeout(() => {
+        // 跳转到其他界面
+        window.location.href = "../html/index.html";
+    }, 1600);
 });
